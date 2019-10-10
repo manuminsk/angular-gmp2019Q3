19dcf45a90f3e5784c307cf38cf1d7badd1c3df5
Andrei Shelenhouski <andrei_shelenhouski@epam.com>
2019-10-10 16:20:16 +0300

    style: change formatting

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import {MatToolbarModule} from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
 import { FooterComponent } from './components/footer/footer.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
