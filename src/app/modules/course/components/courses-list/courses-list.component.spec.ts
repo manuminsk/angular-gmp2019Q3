8d1000376286ec65ace04ad8d89326a3a0de9755
Andrei Shelenhouski <manu.minsk@gmail.com>
2019-11-02 15:43:50 +0300

    feat: fetaure/task-3
    - fix course list component's layout according to responsice approach
    - cover courses list component with unit tests

--- src/app/modules/course/components/courses-list/courses-list.component.spec.ts
+++ src/app/modules/course/components/courses-list/courses-list.component.spec.ts
@@ -1,7 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 
 import { CoursesListComponent } from './courses-list.component';
+import { By } from '@angular/platform-browser';
 
 describe('CoursesListComponent', () => {
   let component: CoursesListComponent;
@@ -23,5 +24,39 @@ describe('CoursesListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render search bar', () => {
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const coursesNodes: NodeListOf<Element> = nativeElement.querySelectorAll('app-courses-list-search');
+
+    expect(coursesNodes.length).toBeTruthy();
+  });
+
+  it('should create proper number of course items', () => {
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const coursesNodes: NodeListOf<Element> = nativeElement.querySelectorAll('app-courses-list-item');
+
+    expect(coursesNodes.length).toBe(component.courses.length);
+  });
+
+  it('should call loadMore method', () => {
+    const debugElement: DebugElement = fixture.debugElement;
+    const btn: DebugElement = debugElement.query(By.css('button.load-more-btn'))
+
+    spyOn(component, "onLoadMore");
+    btn.triggerEventHandler('click', null);
+
+    expect(component.onLoadMore).toHaveBeenCalled();
+  });
+
+  it('should call loadMore method', () => {
+    const debugElement: DebugElement = fixture.debugElement;
+    const btn: DebugElement = debugElement.query(By.css('button.add-course-btn'))
+
+    spyOn(component, "onAddCourse");
+    btn.triggerEventHandler('click', null);
+
+    expect(component.onAddCourse).toHaveBeenCalled();
   });
 });
