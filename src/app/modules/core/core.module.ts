3d9ea288d94ff59ad9a1c0feebd9bf275976f3a3
andrei_shelenhouski@epam.com <manu.minsk@gmail.com>
2019-11-16 21:29:30 +0300

    feat(task-5): implement stub login page component

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
 
 import { SharedModule } from '../shared/shared.module';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, LogoComponent, BreadcrumbsComponent],
-  imports: [
-    SharedModule,
-    MatToolbarModule
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    BreadcrumbsComponent,
+    LoginPageComponent
   ],
+  imports: [SharedModule, MatToolbarModule],
   exports: [
     HeaderComponent,
     BreadcrumbsComponent,
-    FooterComponent
+    FooterComponent,
+    LoginPageComponent
   ]
 })
-export class CoreModule { }
+export class CoreModule {}
