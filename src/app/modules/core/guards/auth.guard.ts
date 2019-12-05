32dde1ecb3f10582692dda4ef11f711402fd449c
Andrei Shelenhouski <andrei_shelenhouski@epam.com>
2019-12-05 13:32:00 +0300

    feat(task-7): resolve comments

--- src/app/modules/core/guards/auth.guard.ts
+++ src/app/modules/core/guards/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import {AuthService} from '../services/auth.service';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +12,8 @@ export class AuthGuard implements CanActivate {
 
   constructor(private readonly authService: AuthService, private readonly router: Router) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
+  public canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
     this.isAuthenticated = this.authService.isAuthenticated();
-
 
     if (!this.isAuthenticated) {
       const url = '/login';
@@ -27,5 +23,4 @@ export class AuthGuard implements CanActivate {
 
     return this.isAuthenticated;
   }
-
 }
