48820a71e57f6fa5d38e1182dec15f63b6b30508
andrei_shelenhouski@epam.com <manu.minsk@gmail.com>
2020-01-14 13:30:21 +0300

    feat(task-11): implement course form inputs as form controls

--- src/app/modules/course/components/course-form/course-form.component.ts
+++ src/app/modules/course/components/course-form/course-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatChipInputEvent } from '@angular/material/chips';
 
@@ -10,17 +12,34 @@ import { Author, Course } from '@course/models/course.class';
   styleUrls: ['./course-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
   @Input() public course: Course;
   @Output() public cancelEvt: EventEmitter<void> = new EventEmitter();
   @Output() public submitEvt: EventEmitter<Course> = new EventEmitter();
 
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
+  public courseForm: FormGroup;
+
   public visible: boolean = true;
   public selectable: boolean = true;
   public removable: boolean = true;
   public addOnBlur: boolean = true;
+
+  public ngOnInit(): void {
+    this.courseForm = new FormGroup({
+      id: new FormControl(this.course.id),
+      name: new FormControl(this.course.name, [Validators.required, Validators.maxLength(50)]),
+      thumbnail: new FormControl(this.course.thumbnail),
+      date: new FormControl(new DatePipe('en-US').transform(this.course.date, 'yyyy-MM-dd'), Validators.required),
+      isTopRated: new FormControl(this.course.isTopRated),
+      // TODO: fix validation
+      length: new FormControl(this.course.length, Validators.required),
+      description: new FormControl(this.course.description, [Validators.required, Validators.maxLength(500)]),
+      // TODO: fix validation
+      authors: new FormControl(this.course.authors, Validators.required)
+    });
+  }
 
   public add(event: MatChipInputEvent): void {
     const input = event.input;
@@ -48,6 +67,6 @@ export class CourseFormComponent {
   }
 
   public onSubmit(): void {
-    this.submitEvt.emit(this.course);
+    this.submitEvt.emit(this.courseForm.value);
   }
 }
