b103b7aecdd8c6af73123075aba20f0eeed82088
andrei_shelenhouski@epam.com <manu.minsk@gmail.com>
2020-01-15 10:50:50 +0300

    feat(task-11): rework search on listing to form control

--- src/app/modules/course/components/courses-list-search/courses-list-search.component.ts
+++ src/app/modules/course/components/courses-list-search/courses-list-search.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-courses-list-search',
@@ -6,11 +7,17 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angul
   styleUrls: ['./courses-list-search.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CoursesListSearchComponent {
+export class CoursesListSearchComponent implements OnInit {
+  public searchTextInput: FormControl;
+
   @Output() findEvt: EventEmitter<string> = new EventEmitter<string>();
   @Output() resetEvt: EventEmitter<void> = new EventEmitter<void>();
 
-  public onChangeInput(event): void {
-    this.findEvt.emit(event.target.value);
+  public ngOnInit(): void {
+    this.searchTextInput = new FormControl();
+  }
+
+  public onChangeInput(): void {
+    this.findEvt.emit(this.searchTextInput.value);
   }
 }
